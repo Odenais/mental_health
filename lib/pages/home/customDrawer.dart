4ab2aa799c79a58f2512244c0f06f345bc3cb914
Odenais <143911892+Odenais@users.noreply.github.com>
2024-09-05 01:42:45 -0600

    menu update

--- lib/pages/home/customDrawer.dart
+++ lib/pages/home/customDrawer.dart
@@ -1,16 +1,46 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:rive/rive.dart' as rive;
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-class CustomDrawer extends StatelessWidget {
-  static late rive.StateMachineController? stateMachineController;
-  static rive.SMIInput<bool>? night, day;
+class CustomDrawer extends StatefulWidget {
+  @override
+  _CustomDrawerState createState() => _CustomDrawerState();
+}
 
-  static void click() {
-    if (day!.value) {
-      night?.change(true);
-    } else {
-      day?.change(true);
+class _CustomDrawerState extends State<CustomDrawer> {
+  StateMachineController? _stateMachineController;
+  SMIBool? _dayInput;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _updateStateBasedOnTime() {
+    final now = DateTime.now();
+    final hour = now.hour;
+
+    // Set dayInput based on the hour
+    if (hour > 7) {
+      _dayInput?.change(true);
+    } else{
+      _dayInput?.change(false);
+    }
+  }
+
+  void _onRiveInit(Artboard artboard) {
+    try {
+      final controller = StateMachineController.fromArtboard(artboard, 'Button_Animation');
+      if (controller != null) {
+        artboard.addController(controller);
+        _stateMachineController = controller;
+        _dayInput = controller.findInput<bool>('Day') as SMIBool?;
+        _updateStateBasedOnTime();
+      } else {
+        print('StateMachineController no inicializado');
+      }
+    } catch (e) {
+      print('Error al inicializar Rive: $e');
     }
   }
 
@@ -18,46 +48,28 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // Contenido principal de la pantalla
         Container(
           color: Colors.white,
         ),
-        // Drawer personalizado que llega hasta la barra de estado
         Positioned(
-          top: 0, // Llega hasta la barra de estado
+          top: 0,
           left: 0,
           right: 0,
           child: Container(
-            width: MediaQuery.of(context).size.width * 0.8, // Ajusta el ancho
-            height: MediaQuery.of(context)
-                .size
-                .height, // Ocupa todo el alto disponible
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height,
             color: Colors.white,
             child: Column(
               children: [
-                // Header con la animación Rive
                 SizedBox(
-                  height: 200, // Altura del header
-                  child: rive.RiveAnimation.asset(
+                  height: 200,
+                  child: RiveAnimation.asset(
                     'assets/menu_header.riv',
-                    stateMachines: const ["Button Animation"],
-                    fit: BoxFit.cover, // Ocupa todo el espacio
+                    fit: BoxFit.cover,
                     alignment: Alignment.center,
-                    onInit: (artBoard) {
-                      stateMachineController =
-                          rive.StateMachineController.fromArtboard(
-                        artBoard,
-                        "Button Animation",
-                      );
-                      if (stateMachineController == null) return;
-                      artBoard.addController(stateMachineController!);
-
-                      night = stateMachineController?.findInput("Night");
-                      day = stateMachineController?.findInput("Day");
-                    },
+                    onInit: _onRiveInit,
                   ),
                 ),
-                // Opciones del menú
                 ListTile(
                   leading: Icon(Icons.account_circle),
                   title: Text('Perfil'),
